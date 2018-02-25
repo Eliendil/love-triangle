3241dd5902f2269c9256a96850abe7c66659bca7
Ilia Okunev <iliaokunev@gmail.com>
2018-02-25 16:32:43 +0300

    love-trangle commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var provenIndexes = new Set();
+    const N = preferences.length;
+    var x =0;
+    for(let i=0; i < N; i++) {
+      if(!provenIndexes.has(i + 1)) {
+        let firstTarget = preferences[i];
+        let secondTarget = preferences[firstTarget - 1];
+        let thirdTarget = preferences[secondTarget - 1];
+        /*
+         * if second target is equal to third target, 
+         * then object loves himself. 
+         */
+        if(thirdTarget != secondTarget && thirdTarget == i + 1) {
+          x++;
+          provenIndexes.add(firstTarget);
+          provenIndexes.add(secondTarget);
+          provenIndexes.add(thirdTarget);
+        }
+      }
+    }
+    return x;
 };
